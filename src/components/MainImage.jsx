3b938cbc8f89a39f9463374cb7cf52ad22e25248
Figby222 <figby222@outlook.com>
2024-11-07 03:57:49 -0800

    Add MainImage Prop Types

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,3 +1,16 @@
-const MainImage = () => {};
+import PropTypes from "prop-types";
+
+const MainImage = ({ useAllData, onClick, selectCharacterPositionPost }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+MainImage.propTypes = {
+    useAllData: PropTypes.func.isRequired,
+    onClick: PropTypes.func.isRequired,
+    selectCharacterPositionPost: PropTypes.func.isRequired,
+}
 
 export default MainImage;
