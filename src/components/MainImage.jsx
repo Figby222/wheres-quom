2006a7b3e6b86878ef6c3343dc3ea001aae5b050
Figby222 <figby222@outlook.com>
2024-11-08 01:13:57 -0800

    Make target box visible if isTargetBoxVisible
    Set target box hidden attribute to the opposite of
    isTargetBoxVisible state

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,17 +1,25 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const MainImage = ({ useAllData, selectCharacterPositionPost }) => {
     const { error, loading, data } = useAllData();
+    const [ isTargetBoxVisible, setIsTargetBoxVisible ] = useState(false);
 
     if (loading) {
         return (<p className="loading">Loading...</p>)
     }
 
+    const onImageClick = (e) => {
+        e.preventDefault();
+
+        setIsTargetBoxVisible(!isTargetBoxVisible);
+    }
+
     return (
         <>
-            <img src="" alt={data.imageAlt} />
+            <img src="" alt={data.imageAlt} onClick={onImageClick} />
             <section className="character-selection">
-                <div role="targetbox"></div>
+                <div role="targetbox" hidden={!isTargetBoxVisible}></div>
                 <ul className="character-selection-ul">
                     { data.characters.map((character) => {
                         return <li key={character.id} className="select-character">
