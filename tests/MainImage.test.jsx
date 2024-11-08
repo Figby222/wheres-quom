e3be27214938406ee6aa79d513bdf77738bea97b
Figby222 <figby222@outlook.com>
2024-11-07 22:44:58 -0800

    Make sure an img is rendered when done loading

--- tests/MainImage.test.jsx
+++ tests/MainImage.test.jsx
@@ -52,4 +52,22 @@ describe("Image loading", () => {
         expect(screen.queryByText(/Loading/i))
             .not.toBeInTheDocument();
     })
+
+    it("Renders image when done loading", () => {
+        const mockUseAllData = getUseAllDataMock(false, false, {
+            imageSrc: "/",
+            imageAlt: "Test Alt Text",
+            characters: [],
+        });
+
+        const mockSelectCharacterPositionPost = vi.fn(() => ({}));
+
+        render(<MainImage useAllData={mockUseAllData} selectCharacterPositionPost={mockSelectCharacterPositionPost} />);
+
+        expect(screen.queryByText(/Loading/i))
+            .not.toBeInTheDocument();
+
+        expect(screen.queryByAltText(/Test Alt Text/i))
+            .toBeInTheDocument();
+    })
 })
