e1a3debd96363473fc02b4383ed9f3dc961088ba
Figby222 <figby222@outlook.com>
2024-11-08 02:05:54 -0800

    Add TargetBox Prop Types

--- src/components/TargetBox.jsx
+++ src/components/TargetBox.jsx
@@ -1,3 +1,16 @@
-const TargetBox = () => {};
+import PropTypes from "prop-types";
 
+const TargetBox = ({ coordinates }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+TargetBox.propTypes = {
+    coordinates: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+    })
+}
 export default TargetBox;
