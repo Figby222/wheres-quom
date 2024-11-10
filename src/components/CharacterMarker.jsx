91ab313cfc06e15eef6144fcc8163e3a6b74dadc
Figby222 <figby222@outlook.com>
2024-11-10 02:16:57 -0800

    Add CharacterMarker Prop Types

--- src/components/CharacterMarker.jsx
+++ src/components/CharacterMarker.jsx
@@ -1,4 +1,16 @@
-const CharacterMarker = () => {};
+import PropTypes from "prop-types";
 
+const CharacterMarker = ({ coordinates }) => {
+    return (
+        <>
+        </>
+    )
+};
+CharacterMarker.propTypes = {
+    coordinates: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+    })
+}
 
 export default CharacterMarker;
