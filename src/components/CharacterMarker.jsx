66d8b4c8708e97c4b94d06b6c0b3a2c35af0ff74
Figby222 <figby222@outlook.com>
2024-11-10 02:39:09 -0800

    Set CharacterMarker style top to dummy value

--- src/components/CharacterMarker.jsx
+++ src/components/CharacterMarker.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const CharacterMarker = ({ coordinates }) => {
     return (
         <>
-            <div role="charactermarker" style={{ position: "absolute", left: `${coordinates.x}px` }}></div>
+            <div role="charactermarker" style={{ position: "absolute", left: `${coordinates.x}px`, top: "4px" }}></div>
         </>
     )
 };
