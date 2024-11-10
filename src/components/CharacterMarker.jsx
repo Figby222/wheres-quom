f17714e244ff4274b3eee7f5cf44a736ccfa9223
Figby222 <figby222@outlook.com>
2024-11-10 02:19:54 -0800

    Add charactermarker div element

--- src/components/CharacterMarker.jsx
+++ src/components/CharacterMarker.jsx
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 const CharacterMarker = ({ coordinates }) => {
     return (
         <>
+            <div role="charactermarker"></div>
         </>
     )
 };
+
 CharacterMarker.propTypes = {
     coordinates: PropTypes.shape({
         x: PropTypes.number.isRequired,
