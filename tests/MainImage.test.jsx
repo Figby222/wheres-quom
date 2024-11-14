54e8de919e5e00437ff98031ba3d91e0ce2c7ccd
Figby222 <figby222@outlook.com>
2024-11-13 23:15:31 -0800

    Make sure name input is only rendered upon win
    Make sure name input is only rendered upon user
    win

--- tests/MainImage.test.jsx
+++ tests/MainImage.test.jsx
@@ -1210,4 +1210,63 @@ describe("High Score Form", () => {
         expect(screen.queryByLabelText(/Name/i))
             .toBeInTheDocument();
     })
+
+    it("Only renders input when user wins", async () => {
+        const mockUseAllData = getUseAllDataMock(false, false, {
+            imageSrc: "/",
+            imageAlt: "Test Alt Text",
+            characters: [
+                {
+                    id: 1,
+                    name: "Comal",
+                },
+                {
+                    id: 2,
+                    name: "quom",
+                }
+            ]
+        });
+
+        const mockSelectCharacterPositionPost = vi.fn(() => ({
+            success: true,
+            characterId: 4,
+            coordinates: {
+                x: "4%",
+                y: "8%",
+            }
+        }));
+
+        render(<MainImage useAllData={mockUseAllData} selectCharacterPositionPost={mockSelectCharacterPositionPost} />);
+
+        vi
+        .spyOn(window.HTMLElement.prototype, "getBoundingClientRect")
+        .mockImplementation(() => ({
+            width: 500,
+            left: 24,
+            right: 0,
+            height: 96,
+            top: 64,
+            bottom: 0,
+        }))
+        const image = screen.queryByAltText("Test Alt Text");
+
+        const user = userEvent.setup();
+        
+        const x = 46;
+        const y = 64;
+
+        await user.pointer({
+            keys: "[MouseLeft]",
+            target: image,
+            coords: { x: x, y: y },
+        })
+
+        const comalButton = screen.queryByText(/quom/i);
+
+
+        await user.click(comalButton);
+
+        expect(screen.queryByLabelText(/Name/i))
+            .not.toBeInTheDocument();
+    })
 })
