ae665c32e4f4a30f4cd69d3c4b481fd5cfd3a564
Figby222 <figby222@outlook.com>
2024-11-16 23:56:48 -0800

    A

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -85,7 +85,7 @@ const MainImage = ({ useAllData, selectCharacterPositionPost, submitScorePut }) 
 
     return (
         <>
-            <section className="leaderboard" aria-label="leaderboard">24:46</section>
+            <section className="leaderboard" aria-label="leaderboard">24:46</section>a
             <dialog open={userDidWin} hidden={!userDidWin}>
                 <form method="PUT" onSubmit={onWinnerFormSubmission} hidden={!userDidWin}>
                     <label htmlFor="name" hidden={!userDidWin}>
