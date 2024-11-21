286948d1ad7533196e56d5dcb781a3ec246a68e2
Figby222 <figby222@outlook.com>
2024-11-21 00:54:02 -0800

    Make sure game error message only shown on error

--- tests/MainImage.test.jsx
+++ tests/MainImage.test.jsx
@@ -2304,3 +2304,92 @@ describe("Winner submission errors", () => {
             .toBeInTheDocument();
     })
 })
+
+it("Doesn't show error message on successful submission", async () => {
+    const mockUseAllData = getUseAllDataMock(false, false, {
+        imageSrc: "/",
+        imageAlt: "Test Alt Text",
+        characters: [
+            {
+                id: 1,
+                name: "Comal",
+            },
+            {
+                id: 2,
+                name: "quom",
+            }
+        ],
+        leaderboardPlayers: [
+            {
+                id: 3,
+                name: "Comal",
+                completionTime: "24:24"
+            },
+            {
+                id: 4,
+                name: "quom",
+                completionTime: "46:46"
+            }
+        ]
+    });
+
+    
+
+    const mockSelectCharacterPositionPost = vi.fn(() => ({
+        highScore: true,
+        success: true,
+        characterId: 4,
+        coordinates: {
+            x: "4%",
+            y: "8%",
+        }
+    }));
+    
+    const mockSubmitScorePut = vi.fn(() => ({
+        status: 200
+    }))
+
+    render(<MainImage useAllData={mockUseAllData} selectCharacterPositionPost={mockSelectCharacterPositionPost} submitScorePut={mockSubmitScorePut} />);
+
+        
+
+    vi
+    .spyOn(window.HTMLElement.prototype, "getBoundingClientRect")
+    .mockImplementation(() => ({
+        width: 500,
+        left: 24,
+        right: 0,
+        height: 96,
+        top: 64,
+        bottom: 0,
+    }))
+    const image = screen.queryByAltText("Test Alt Text");
+
+    const user = userEvent.setup();
+    
+    const x = 46;
+    const y = 64;
+
+    await user.pointer({
+        keys: "[MouseLeft]",
+        target: image,
+        coords: { x: x, y: y },
+    })
+
+    const comalButton = screen.queryByText(/quom/i);
+
+
+    await user.click(comalButton);
+    
+    const nameInput = screen.queryByRole("textbox", { name: /Name/i });
+
+    await user.type(nameInput, "Test Name");
+
+    const submitButton = screen.queryByRole("button", { name: /Submit/i });
+
+
+    await user.click(submitButton);
+
+    expect(screen.queryByText(/You must finish the game before submitting your score/i))
+        .not.toBeInTheDocument();
+})
