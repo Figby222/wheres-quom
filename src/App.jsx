07b85290b5ed01c6d165a70afd925f81e9e89c72
Figby222 <figby222@outlook.com>
2025-02-05 15:54:29 -0800

    Render MainImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import MainImage from "./components/MainImage.jsx";
+import MainImageUtils from "./util/MainImageUtils.jsx";
+import useAllData from "./util/useAllData.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MainImage useAllData={useAllData} selectCharacterPositionPost={MainImageUtils.selectCharacterPositionPost} submitScorePut={MainImageUtils.submitScorePut} />
     </>
   )
 }
