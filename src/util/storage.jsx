5f2d934bb24f86361cdbdb5069cdca800271db43
Figby222 <figby222@outlook.com>
2025-02-14 15:43:30 -0800

    Add getBearerToken()

--- src/util/storage.jsx
+++ src/util/storage.jsx
@@ -1,4 +1,10 @@
 const storeBearerToken = (bearerToken) => {
     localStorage.setItem("bearerToken", bearerToken);
 }
-export { storeBearerToken };
+
+const getBearerToken = () => {
+    const token = localStorage.getItem("bearerToken");
+
+    return token;
+}
+export { storeBearerToken, getBearerToken };
